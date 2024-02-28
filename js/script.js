71a0b27e6d4aa687d00c6b7dad7aa7b180cfe00f
bezkot1508 <daminovbekzod13@gmail.com>
2024-02-28 21:56:47 +0500

    adedd new way offer, and slider

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 window.addEventListener('DOMContentLoaded', () => {
 
     // *************  TABS  *******************
@@ -44,7 +42,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     setTimeout( () => {
         loaderWrapper.style.display = 'none'
-    },1500)
+        // TODO 500 change
+    },500)
 
 
      // *************  Timer  *******************
@@ -345,4 +344,123 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal()
         }, 3000)
     }
+
+    // SLIDER 
+    const slides = document.querySelectorAll(".offer__slide"),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current'),
+        slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+        slidesInner = document.querySelector(".offer__slider-inner"),
+        width = window.getComputedStyle(slidesWrapper).width
+        
+        console.log(width);
+
+    let slideIndex = 1
+    let offset = 0
+
+
+    if (total < 10) {
+        total.textContent = `0${slides.length}`
+        current.textContent = `0${slideIndex}`
+    } else {
+        total.textContent = slides.length
+        current.textContent = slideIndex
+    }
+
+    slidesInner.style.width = 100 * slides.length + '%'
+    slidesInner.style.display = 'flex'
+    slidesInner.style.transition = "all .5s ease"
+    slidesWrapper.style.overflow = 'hidden'
+
+    slides.forEach(slide => {
+        slide.style.width = width
+    })
+
+    next.addEventListener('click', () => {
+        if ( offset === +width.slice(0, width.length - 2) * (slides.length -1)) {
+            offset = 0
+        } else {
+            offset += +width.slice(0, width.length -2)
+        }
+        slidesInner.style.transform = `translateX(-${offset}px)`
+
+        if ( slideIndex === slides.length) {
+            slideIndex = 1
+        } else {
+            slideIndex++
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`
+        } else {
+            current.textContent = slideIndex
+        }
+    })
+
+    prev.addEventListener('click', () => {
+        if ( offset === 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length -1)
+        } else {
+            offset -= +width.slice(0, width.length -2)
+        }
+        slidesInner.style.transform = `translateX(-${offset}px)`
+
+        
+        if ( slideIndex === 1) {
+            slideIndex = slides.length
+        } else {
+            slideIndex--
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`
+        } else {
+            current.textContent = slideIndex
+        }
+    })
+
+
+    
+//     showSlides(slideIndex)
+
+
+//     function showSlides(index) { 
+
+//         if (index > slides.length) {
+//             slideIndex = 1
+//         }
+
+//         if (index < 1) {
+//             slideIndex = slides.length
+//         }
+
+//         slides.forEach(slide => slide.style.display = 'none')
+        
+//         slides[slideIndex - 1].style.display = 'block'
+
+//         if (total < 10) {
+//             current.textContent = `0${slideIndex}`
+//         } else {
+//             current.textContent = slideIndex
+//         }
+
+//         if (total < 10) {
+//             total.textContent = `0${slides.length}`
+//         } else {
+//             total.textContent = slides.length
+//         }
+//     }
+
+//     function moveSlides (index) {
+//         showSlides(slideIndex += index)
+//     }
+
+//     prev.addEventListener('click', () => {
+//         moveSlides(- 1)
+//     })
+//     next.addEventListener('click', () => {
+//         moveSlides(+ 1)
+//     })
 })
